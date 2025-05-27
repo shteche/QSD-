dbb0b1ccf856a6825efe163bfa1511706ed855a6
猫猫酱 <shteche@foxmail.com>
2025-05-27 10:35:21 +0800

    Update verify.sh

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -1,1 +1,13 @@
+#!/system/bin/sh
 
+. /data/adb/QSD++/common/util_verify.sh
+
+FILES_TO_VERIFY=(
+  "META-INF/com/google/android/update-binary"
+  "scripts/post-fs-data.sh"
+  "scripts/service.sh"
+)
+
+for FILE in "${FILES_TO_VERIFY[@]}"; do
+  verify_file "$ZIPFILE" "$FILE"
+done
